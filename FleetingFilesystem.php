5f3ce579cbd100767e1deb65d40ff3a0cbd05a55
Anthony Colangelo <colangelo@happycog.com>
2012-02-23 15:48:30 -0500

    Updated CacheKey names to include file name.

--- FleetingFilesystem.php
+++ FleetingFilesystem.php
@@ -3,7 +3,7 @@
 class Twig_Loader_FleetingFilesystem extends Twig_Loader_filesystem {
 	
 	public function getCacheKey($name) {
-		return filemtime($this->findTemplate($name));
+		return $this->findTemplate($name) . filemtime($this->findTemplate($name));
 	}
 
 	public function isFresh($name, $time) {
